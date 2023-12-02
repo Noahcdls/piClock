5782c2473fc198a957d6b3a1b1e446b6f5afc3cd
noahcdls <noahcdls@gmail.com>
2023-12-02 14:51:07 -0800

    basic tutorial for GPIO

---
+++ basicGpio.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/ioctl.h>
+#include <linux/spi/spidev.h>
+#include <linux/gpio.h>
+
+int main(){
+    int fd;
+    struct gpiohandle_request led, button;
+    struct gpiohandle_data data;
+
+    fd = open("/dev/gpiochip0", O_RDWR);
+    if(fd < 0)
+    {
+        printf("Failed to open gpio\n");
+        return -1;
+    }
+    led.flags = GPIOHANDLE_REQUEST_OUTPUT;
+    strcpy(led.consumer_label, "LED");
+    memset(led.default_values, 0, sizeof(led.default_values));
+    led.lines = 1;
+    led.lineoffsets[0] = 16;
+
+    if(ioctl(fd, GPIO_GET_LINEHANDLE_IOCTL, &led) < 0)
+    {
+        printf("Error setting GPIO 16 to output\n");
+        close(fd);
+        return -1;
+    }
+    return 0;
+    
+
+
+}
